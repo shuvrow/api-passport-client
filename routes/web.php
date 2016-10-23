f018ef5a5a7dfbadfdd3abb24df5b110d99beae4
shuvrow <shuvrow@gmail.com>
2016-10-23 09:47:24 -0400

    new access using assecss token is configured

--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,41 @@ Route::get('/callback', function (Request $request) {
         ],
     ]);
 
-    return json_decode((string) $response->getBody(),true);
-    
+    json_decode((string) $response->getBody(),true);
 
 
 });
+
+Route::get('users',function(){
+    $http = new GuzzleHttp\Client;
+    $accessToken='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjdjOGI3YzJjZDllOTgwMjhhYWMzMjkxNjA4MjM1MWQ3OWMzNjM5MmU2N2QwMTBjOGMwYTE0OTIwZWRiY2JiODY0NDRhYzk4YmUzZGIxMWRkIn0.eyJhdWQiOiIxMyIsImp0aSI6IjdjOGI3YzJjZDllOTgwMjhhYWMzMjkxNjA4MjM1MWQ3OWMzNjM5MmU2N2QwMTBjOGMwYTE0OTIwZWRiY2JiODY0NDRhYzk4YmUzZGIxMWRkIiwiaWF0IjoxNDc3MjIxODEwLCJuYmYiOjE0NzcyMjE4MTAsImV4cCI6NDYzMjg5NTQxMCwic3ViIjoiMyIsInNjb3BlcyI6W119.Ixw48yPii65GG5TC2eTh2i5VfU6dMsYMVl0iUWBHM2y5xVtSB-VOq6RBVdImDi6gaBJxYcENGyAWTyDNI5tE4Fkfc-WDIqDZRnm_7g0SRcR66ouBe-DDfl23chSlFq23zOCkB7tYx07-SxgKMqunvi_HfxfvvdSIvsoAygXJrjG_egUFQ78N9Z3aN_4d4D7Ze2oCpX146tQ0srAQNw0_0SG3s9YOWNsgyVRE0znViSO-iss-JWk8Zi6YP67y7WnfijIvKuIBl9Bi52-YsF5_-5-9eTzDoPuEFpRHakKnvzMeHucKimSaaHmMwAEx3wteYFvaXWu7IUs3UE2mfOJEvvn1SZguXscH_2G-ZVNsbLQ52Wy61MDw1alm00qiwdD4vdl2UPGnPDLz4fjpaeOKZfyQxzYuKhFqlRCHR3p_3Sfrx1BkSDwDwsZEfevP3bsY6GX4Qraws8z4GJgaWfVMbva8RjkEkSemQPx1fBTWtQyDcg_vqdDDClEVj-I9FD2sb6Ay88CR9_iRHKK5NVSDsDUvnof6ht0gOhqVrRvWvBUwG2gG9qSMt4Ti6yN7Khlco3D1zAkCeRg3hmVmI93uQ8WEVfVR1Oj9AckGoLPy-S_6oC6HtXdDoTIFxCLrupNgxAIQU7AFBkZUwqUfinF63uOgArerWTmon5SS7A86OiA';
+
+
+    $response = $http->get('http://localhost:8000/api/user', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$accessToken,
+        ],
+    ]);
+
+    dd(json_decode((string) $response->getBody(),true));
+
+
+});
+Route::get('products',function(){
+    $http = new GuzzleHttp\Client;
+    $accessToken='eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjdjOGI3YzJjZDllOTgwMjhhYWMzMjkxNjA4MjM1MWQ3OWMzNjM5MmU2N2QwMTBjOGMwYTE0OTIwZWRiY2JiODY0NDRhYzk4YmUzZGIxMWRkIn0.eyJhdWQiOiIxMyIsImp0aSI6IjdjOGI3YzJjZDllOTgwMjhhYWMzMjkxNjA4MjM1MWQ3OWMzNjM5MmU2N2QwMTBjOGMwYTE0OTIwZWRiY2JiODY0NDRhYzk4YmUzZGIxMWRkIiwiaWF0IjoxNDc3MjIxODEwLCJuYmYiOjE0NzcyMjE4MTAsImV4cCI6NDYzMjg5NTQxMCwic3ViIjoiMyIsInNjb3BlcyI6W119.Ixw48yPii65GG5TC2eTh2i5VfU6dMsYMVl0iUWBHM2y5xVtSB-VOq6RBVdImDi6gaBJxYcENGyAWTyDNI5tE4Fkfc-WDIqDZRnm_7g0SRcR66ouBe-DDfl23chSlFq23zOCkB7tYx07-SxgKMqunvi_HfxfvvdSIvsoAygXJrjG_egUFQ78N9Z3aN_4d4D7Ze2oCpX146tQ0srAQNw0_0SG3s9YOWNsgyVRE0znViSO-iss-JWk8Zi6YP67y7WnfijIvKuIBl9Bi52-YsF5_-5-9eTzDoPuEFpRHakKnvzMeHucKimSaaHmMwAEx3wteYFvaXWu7IUs3UE2mfOJEvvn1SZguXscH_2G-ZVNsbLQ52Wy61MDw1alm00qiwdD4vdl2UPGnPDLz4fjpaeOKZfyQxzYuKhFqlRCHR3p_3Sfrx1BkSDwDwsZEfevP3bsY6GX4Qraws8z4GJgaWfVMbva8RjkEkSemQPx1fBTWtQyDcg_vqdDDClEVj-I9FD2sb6Ay88CR9_iRHKK5NVSDsDUvnof6ht0gOhqVrRvWvBUwG2gG9qSMt4Ti6yN7Khlco3D1zAkCeRg3hmVmI93uQ8WEVfVR1Oj9AckGoLPy-S_6oC6HtXdDoTIFxCLrupNgxAIQU7AFBkZUwqUfinF63uOgArerWTmon5SS7A86OiA';
+
+
+    $response = $http->get('http://localhost:8000/api/products', [
+        'headers' => [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer '.$accessToken,
+        ],
+    ]);
+
+    dd(json_decode((string) $response->getBody(),true));
+
+
+});
+
