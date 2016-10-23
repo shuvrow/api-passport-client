799676c9ab8bafe143a5397ac83b343d2a967768
shuvrow <shuvrow@gmail.com>
2016-10-23 01:33:39 -0400

    project as acting client for project laravel API authentication using Passport in l5.3

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
-    return view('welcome');
+    $query = http_build_query([
+        'client_id' => 12,
+        'redirect_uri' => 'http://localhost:9000/callback',
+        'response_type' => 'code',
+        'scope' => '',
+    ]);
+
+    return redirect('http://localhost:8000/oauth/authorize?'.$query);
+});
+
+Route::get('/callback', function (Request $request) {
+
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => 12,
+            'client_secret' => 'rfkixBY5v3SwnqwRM570yKHfgQMRNyNDd1WgOxWP',
+            'redirect_uri' => 'http://localhost:9000/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(),true);
+    
+
+
 });
